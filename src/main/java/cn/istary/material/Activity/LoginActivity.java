8e69b13f136139d59a8388ab0dc5d8a5b4fc4b79
JuiceYY <xryuan@stu.xidian.edu.cn>
2018-10-05 23:46:01 +0800

    modify the Login activity

--- src/main/java/cn/istary/material/Activity/LoginActivity.java
+++ src/main/java/cn/istary/material/Activity/LoginActivity.java
@@ -2,6 +2,7 @@ package cn.istary.material.Activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,8 @@ public class LoginActivity extends BaseActivity {
     private EditText usernameEdit;
     private EditText passwdEdit;
     private Button loginButton;
+    private TextInputLayout tlUsername;
+    private TextInputLayout tlPasswd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +25,46 @@ public class LoginActivity extends BaseActivity {
         usernameEdit = findViewById(R.id.login_username_edit);
         passwdEdit = findViewById(R.id.login_passwd_edit);
         loginButton = findViewById(R.id.login_button);
+        tlUsername = findViewById(R.id.tl_username);
+        tlPasswd = findViewById(R.id.tl_passwd);
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String username = usernameEdit.getText().toString();
-                String passwd = passwdEdit.getText().toString();
-                if(username.equals("yxr") && passwd.equals("123456")){
+                //String username = usernameEdit.getText().toString();
+                //String passwd = passwdEdit.getText().toString();
+                String username = tlUsername.getEditText().getText().toString();
+                String passwd = tlPasswd.getEditText().getText().toString();
+
+                if(!validateUsername(username)){
+                    tlUsername.setErrorEnabled(true);
+                    tlUsername.setError("Please input correct username");
+                }else if(!validatePasswd(passwd)){
+                    tlPasswd.setErrorEnabled(true);
+                    tlPasswd.setError("The password should between 6-12 bits");
+                }else if(username.equals("yxr") && passwd.equals("1234567")){
+                    tlUsername.setErrorEnabled(false);
+                    tlPasswd.setErrorEnabled(false);
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
 //                    finish();
                 }else{
+                    tlUsername.setErrorEnabled(false);
+                    tlPasswd.setErrorEnabled(false);
                     Toast.makeText(LoginActivity.this, "username or passwd wrong", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
+    }
+
+    private boolean validatePasswd(String passwd){
+        return (passwd.length() > 6 && passwd.length() < 16) ;
+    }
+
+    private boolean validateUsername(String name){
+        return (name.equals("yxr"));
     }
 
 
